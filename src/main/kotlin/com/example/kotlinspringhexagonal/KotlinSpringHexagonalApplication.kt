203fc736b23d912105c548bddb54f0e3ee8c457a
drevispas <yonggeon.kim@gmail.com>
2023-01-04 17:35:58 +0900

    initial project

---
+++ src/main/kotlin/com/example/kotlinspringhexagonal/KotlinSpringHexagonalApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinspringhexagonal
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinSpringHexagonalApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinSpringHexagonalApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/kotlinspringhexagonal/KotlinSpringHexagonalApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.kotlinspringhexagonal
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinSpringHexagonalApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
