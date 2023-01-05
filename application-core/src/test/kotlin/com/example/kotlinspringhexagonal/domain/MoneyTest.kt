f90b4eaafbda77b3245c93026fe40e1b5a4fb1b0
drevispas <yonggeon.kim@gmail.com>
2023-01-06 02:39:33 +0900

    test: add comments

--- application-core/src/test/kotlin/com/example/kotlinspringhexagonal/domain/MoneyTest.kt
+++ application-core/src/test/kotlin/com/example/kotlinspringhexagonal/domain/MoneyTest.kt
@@ -10,16 +10,20 @@ import io.kotest.matchers.shouldBe
  */
 class MoneyTest : DescribeSpec() {
     init {
+        // describe: # 뒤에 member fun 이름을 써 준다.
         describe("#plus") {
+            // context: 테스트하려고 하는 상황을 기술한다.
             context("when adding two money") {
                 beforeEach {
                     // setUp. teadDown을 beforeEach, afterEach로 표현한다.
                 }
+                // it: should로 시작하는 예상 결과를 기술한다.
                 it("should return the sum of two money") {
                     val amount1 = 12L
                     val amount2 = 34L
                     val actual = Money(amount1) + Money(amount2)
                     val expected = Money(amount1 + amount2)
+                    // assertJ와 같은 assertion을 kotest가 kotlin DSL을 이용하여 제공해준다.
                     actual shouldBe expected
                 }
             }
