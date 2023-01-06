400654d2c94c7a80b2dd706a3406c45250c8d8c8
drevispas <yonggeon.kim@gmail.com>
2023-01-07 02:52:19 +0900

    feat: setup kover

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 }
 
 // Root project에는 koverMergedXxx 태스크들을 추가한다.
+// `./gradlw koverMergedReport` 를 실행해서 커버리지 보고서를 볼 수 있다.
 // https://kotlin.github.io/kotlinx-kover/
 koverMerged {
     enable()
